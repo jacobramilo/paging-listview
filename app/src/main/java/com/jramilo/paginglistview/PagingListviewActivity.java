674d69e27206bef18e18cf0c400e04e68bb17be2
jacobramilo <32914079+jacobramilo@users.noreply.github.com>
2017-11-08 11:40:08 +1100

    Initial commit

---
+++ app/src/main/java/com/jramilo/paginglistview/PagingListviewActivity.java
@@ -0,0 +1,13 @@
+package com.jramilo.paginglistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagingListviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paging_listview);
+    }
+}
